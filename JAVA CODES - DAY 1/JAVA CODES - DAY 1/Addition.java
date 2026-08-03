032b6094750f00d6f576f6c872ed005a45393da9
ansh-verma-07 <rewavrema@gmail.com>
2026-08-03 23:45:33 +0530

    Add files via upload

---
+++ JAVA CODES - DAY 1/JAVA CODES - DAY 1/Addition.java
@@ -0,0 +1,20 @@
+ 
+    public class Addition
+    {
+        public static void main(String [] args)
+        {
+             int a, b, c;
+
+              a = 100;
+              b = 200;
+              c = a+b;
+
+            System.out.println("\n Addition = "+c);
+
+     System.out.println("\n Addition of "+a+" and "+b+" is "+c);
+        } 
+    }
+
+
+
+
---
+++ JAVA CODES - DAY 1/JAVA CODES - DAY 1/IfDemo6.java
@@ -0,0 +1,38 @@
+/*
+   Q1] HOMEWORK
+
+        if(per < 40)   = FAIL
+        else if(per < 50)   = D GRADE
+        else if(per < 60)   = C GRADE
+        else if(per < 70)   = B Grade
+        else if(per < 80)   = B+ GRADE     
+        else if(per < 90)   = A GRADE
+        else if(per <= 100) = A+ Grade
+        else
+             INVALID INPUT
+------------------------------------------------
+ Q2] WAP to check LEAP YEAR 
+
+       year = 2000  LEAP YEAR
+       year = 2001  NOT A LEAP YEAR
+*/
+
+      import java.util.Scanner;
+
+    public class IfDemo6
+    {
+        public static void main(String [] args)
+        {
+            Scanner s = new Scanner(System.in);   
+
+
+
+
+        } 
+    }
+
+
+
+
+
+
